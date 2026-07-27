201ee42e4f24c7f4ddc41e89d22cc414433e0590
Gundekari-Srinidhi <gundekarisrinidhi@gmail.com>
2026-07-27 10:55:38 +0530

    Time: 32 ms (17.54%), Space: 20.5 MB (61.69%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -11,11 +11,25 @@ class Solution:
             if v==1:
                 return k
         '''
-
+        '''
         val = sorted(nums)
         for i in range(0,len(val)-1,3):
             if val[i] != val[i+1]:
                 return val[i]
         return val[-1]
+        '''
+        res = 0
+        for i in range(32):
+            c = 0
+            for num in nums:
+                if (num>>i)&1:
+                    c += 1
+            if c%3 != 0:
+                res |= 1<<i
+        if res >= 2**31:
+            res -= 2**32
+        return res
+
+
             
         
