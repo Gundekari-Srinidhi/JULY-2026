d82451534d22dcbb500fea3c70431b17fc07f12e
Gundekari-Srinidhi <gundekarisrinidhi@gmail.com>
2026-07-27 11:41:20 +0530

    Time: 3 ms (95%), Space: 20 MB (94%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,5 +1,6 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
+        '''
         l=[]
         for i in range(n+1):
             count=0
@@ -9,5 +10,10 @@ class Solution:
                 i = i>>1
             l.append(count)
         return l
+        '''
+        ans = [0]*(n+1)
+        for i in range(1,n+1):
+            ans [i] = ans[i>>1]+(i&1)
+        return ans
 
         
